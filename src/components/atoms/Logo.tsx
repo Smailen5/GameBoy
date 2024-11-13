64f8442fd324745f53e0c2629239babb7927819e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-13 22:05:36 +0100

    refactor: :sparkles: crea componente
    creo componente logo

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,10 @@
+export const Logo = () => {
+  return (
+    <div className="font-bold capitalize text-blue-900 mt-1">
+      nintendo{" "}
+      <span className="font-semibold uppercase">
+        game boy<sub>&trade;</sub>
+      </span>
+    </div>
+  );
+};
